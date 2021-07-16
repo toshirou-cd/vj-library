9efa0c4b9ba859e78e3a58fb536317f13ec41fe8
AnhNH <toshirou.ha@gmail.com>
2021-07-16 16:32:11 +0700

    "add Myrequest page"

--- src/app/pages/patrons/MyRequest.tsx
+++ src/app/pages/patrons/MyRequest.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Menu,MenuItem, Search, Tab, Table, TabPane,Segment, TableHeader, Input, MenuMenu } from 'semantic-ui-react'
+import BorrowRequestList from './BorrowRequestList'
+import './test.css'
+
+const panes = [
+    {
+      menuItem: 'Borrow Request',
+      render: () => <BorrowRequestList/>,
+    },
+    {
+      menuItem: 'Extend Request',
+      render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane>,
+    }
+  ]
+  
 
 const MyRequest:React.FC = () => {
-    return (
-        <div>
-                        <h1>This is My Request</h1>
 
-        </div>
+    const [activeName, setActiveName] = useState<string>('Borrow Request')
+
+
+    // const handleOnChange = ({e:any}) => {
+    //       setActiveName(e.target.name)
+        
+    // }
+    return (
+        <Container className="RequestContainer">
+            <Menu secondary pointing >
+                <MenuMenu>
+                    <MenuItem  name='Borrow Request'
+                               active={activeName === 'Borrow Request'}
+                               onClick={()=> setActiveName("Borrow Request")} />
+                    <MenuItem  name='Extend Request'
+                                 active={activeName === 'Extend Request'}
+                                 onClick={() => setActiveName("Extend Request")}/>
+                    
+                </MenuMenu>
+                <Menu.Menu position='right'>
+                    <Search />
+                </Menu.Menu>
+            </Menu>
+            <Segment>
+                <BorrowRequestList/>
+            </Segment>
+           {/* <Tab menu={{ secondary: true, pointing: true }} panes={panes} renderActiveOnly>
+               
+            </Tab>          */}
+        </Container>
+      
+        
     )
 }
 
