a101a4c401ef0fdabf5ed71fabaed083544f89c9
LongNH <LongNH@SE140159>
2021-07-19 14:20:44 +0700

    Library and My Books

--- src/app/pages/patrons/MyBooks.tsx
+++ src/app/pages/patrons/MyBooks.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-const MyBooks:React.FC = () => {
+import React, { useState } from 'react';
+import { Container, Menu,MenuItem, Search ,Segment, MenuMenu } from 'semantic-ui-react';
+import BorrowingBookList from "./BorrowingBookList";
+import ExpiredBookList from './ExpiredBookList';
+
+const panes = [
+    {
+      menuItem: 'Borrowing',
+      render: () => <BorrowingBookList/>,
+    },
+    {
+      menuItem: 'Expired',
+      render: () => <ExpiredBookList/>,
+    }
+  ]
+
+const MyBooks:React.FC= () => {
+    const [activeName, setActiveName] = useState<string>('Borrowing')
     return (
-        <div>
-                       <h1>This is Main Book</h1>
- 
-        </div>
+        <Container className="BooksContainier">
+            <Menu secondary pointing >
+                <MenuMenu>
+                    <MenuItem  name='Borrowing'
+                               active={activeName === 'Borrowing'}
+                               onClick={()=> setActiveName("Borrowing")} />
+                    <MenuItem  name='Expired'
+                                 active={activeName === 'Expired'}
+                                 onClick={() => setActiveName("Expired")}/>
+                    
+                </MenuMenu>
+                <Menu.Menu position='right'>
+                    <Search />
+                </Menu.Menu>
+            </Menu>
+            <Segment>
+                {activeName === "Borrowing" ? <BorrowingBookList/>: <ExpiredBookList/>}
+            </Segment>
+        </Container>
     )
 }
 
